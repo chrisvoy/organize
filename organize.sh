4e699282cb24f85382f26ce90789cf360afc5e1b
chrisvoy <aerandiran@gmail.com>
2015-02-03 10:09:07 +1100

    complete initial

--- organize.sh
+++ organize.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 SOURCEDIR=$1"*"
-
 DESTDIR=$2
+MONTHS=(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
 
 for file in $SOURCEDIR
 do
-	echo "Copying file: $file..."
+	
 	YEAR=`stat -f "%Sm" -t "%Y" $file`
-	MONTH=`stat -f "%Sm" -t "%m" $file`
+	MONTHNO=`stat -f "%Sm" -t "%m" $file`
+	MONTH=${MONTHS[$MONTHNO-1]}
 
 	DEST=$DESTDIR$YEAR"/"
-	if [ -d "$DEST" ]; then
-		echo "Exists"
+	if [ ! -d "$DEST" ]; then
+		mkdir $DEST
+	fi
+	DEST=$DESTDIR$YEAR"/"$MONTH"/"
+	if [ ! -d "$DEST" ]; then
+		mkdir $DEST
+	fi
+
+	filename=$(basename "$file")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+	#if file exists in destination
+	if [[ -e $DEST$filename"."$extension ]] ; then
+	    i=1
+	    while [[ -e $DEST$filename"-"$i"."$extension ]] ; do
+	        let i++
+	    done
+	    name=$filename"-"$i"."$extension
+	    #echo "Copying "$file" to "$DEST$name
+		cp -v $file $DEST$name
+	else
+		#echo "Copying "$file" to: "$DEST
+		cp -v $file $DEST
 	fi
 done
